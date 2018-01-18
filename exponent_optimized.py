3e3a4624ddd3d571714981103f540ab6a87d6749
never2average <31365087+never2average@users.noreply.github.com>
2018-01-18 11:38:41 +0530

    Add files via upload

--- exponent_optimized.py
+++ exponent_optimized.py
@@ -1,23 +1,36 @@
 import math
-x=input()          # the base needs to be a 3 digit number atleast
-y=input()
-if x=='1' or x=='0':
-    print (x)
-elif y=='1':
-    print(x)
-elif y=='0':
-    print("1")
-else:
-    inter1=float(y)*(len(x)-1+math.log10(float(x[0]+"."+x[1]+x[2])))
-    print(inter1)
-    if inter1>0:
-        power=math.floor(inter1)
-        mantissa=inter1-power
-        answer=str(10**mantissa)+"*10^"+str(power)
-    elif inter1<0:
-        power=math.floor(inter1)
-        print(power)
-        mantissa=inter1-power
-        answer=str(10**mantissa)+"*10^"+str(power)        
 
-    print(answer)
+x=input()
+n=len(str(int(float(x))))
+y=input()
+
+if x=='1' or x=='0':
+    print (x)
+elif y=='1':
+    print(x)
+elif y=='0':
+    print("1")
+else:
+    if n>=3:
+       a=float(x[0]+"."+x[1]+x[2])
+    elif n==2:
+       a=float(x[0]+"."+x[1]+"0")
+    elif n==1:
+       a=float(x[0]+".00")
+
+    inter1=float(y)*(n-1+math.log10(a))
+    power=math.floor(inter1)
+    mantissa=inter1-power
+    answer=str(10**mantissa)+"*10^"+str(power) 
+    print(answer)
+
+"""
+This program has been created in order to 
+calculate very very large exponentials
+and has a time complexity o(1).
+I have used the lpgarithmic approach for 
+the process and instead of finding the antilog 
+of the whole number i have divided it into mantissa
+and power in order to only find antilog of mantissa
+and therefore draatically cutting down operations.
+"""
