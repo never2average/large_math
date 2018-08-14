eb834ef4edcde0597686c338ab67026f457c0807
Priyesh <priyesh.sriv2017@gmail.com>
2018-08-14 11:20:32 +0530

    Author name added

--- xor_neural network.py
+++ xor_neural network.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import random
-
+__author__="Priyesh Srivastava"
+#print(__author__)
 
 """
 Creating the neural network for an xor gate trained by back propogation
